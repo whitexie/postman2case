ea97ad88b561f892beaaf15718645a774cba8a18
xiebo <shieber@163.com>
2020-01-23 20:26:14 +0800

    filter htmlcov

--- demo.py
+++ demo.py
@@ -1,13 +1,35 @@
-from postman2case.core import PostmanParser
+#encoding: utf-8
+import io
 
+from postman2case import __version__
+from setuptools import find_packages, setup
 
+with io.open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
 
-if __name__ == '__main__':
-    output_file_type = "json"
-    output_dir = '.'
-    postman_testcase_file = './tester_postman_collection.json'
-
-    postman_parser = PostmanParser(postman_testcase_file)
-    parse_result = postman_parser.parse_data()
-    postman_parser.save(parse_result, output_dir, output_file_type=output_file_type)
-
+setup(
+    name='postman2case',
+    version=__version__,
+    description='Convert POSTMAN data to JSON testcases for HttpRunner.',
+    long_description=long_description,
+    author='luguo',
+    author_email='hluguoj@163.com',
+    url='https://github.com/HttpRunner/postman2case',
+    license='MIT',
+    packages=find_packages(exclude=['test.*', 'test']),
+    package_data={},
+    keywords='postman converter json',
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    entry_points={
+        'console_scripts': [
+            'postman2case=postman2case.cli:main'
+        ]
+    }
+)
