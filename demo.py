711eb7918a547c0db94a82f6289f2ad2f3936e37
xiebo <shieber@163.com>
2020-02-14 14:50:13 +0800

    remove coveralls

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     output_dir = '.'
     postman_parser = PostmanParser('./tests/data/open_wechat.postman_collection.json')
-    output_file_type = 'yaml'
+    output_file_type = 'yml'
 
     parse_result = postman_parser.parse_data()
     postman_parser.save(parse_result, output_dir, output_file_type=output_file_type)
